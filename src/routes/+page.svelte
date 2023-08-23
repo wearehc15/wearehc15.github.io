92576529b0bc99a20f506d44059d49739c1bd182
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-08-23 15:36:08 +0200

    wip: coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,26 @@
 </script>
 
 <div class="app">
-	<h1 class="text-3xl font-bold text-center m-4 md:m-6">Welcome HC15Alliance 🌼</h1>
+	<!-- <header class="p-4 md:p-6">
+    <a href="/" class="is-abril tracking-wide text-orange-900 text-xl">HC15</a>
+  </header> -->
 
-	<main>
-		<slot />
+	<main class="flex items-center justify-center h-screen">
+    <div>
+      <h1 class="text-8xl tracking-wide text-orange-900 mb-4">HC15 Alliance</h1>
+      <p class="text-2xl">We are HC15 - a collaborative alliance of entrepreneurs</p>
+    </div>
+		<!-- <slot /> -->
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>© 2023 HC15Alliance - Coming soon...</p>
 	</footer>
 </div>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background: rgb(254,232,206);
+    background: linear-gradient(45deg, rgba(254,232,206,1) 0%, rgba(255,241,223,1) 100%);
   }
 </style>
