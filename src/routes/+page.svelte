eb853aef875e1d6cde14fea24100a7ee45afc2fb
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-08-23 14:05:51 +0200

    feat: add Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,56 @@
-<h1>Welcome HC15Alliance</h1>
+<script>
+	import '../app.css';
+</script>
 
-<p>Coming soon...</p>
+<div class="app">
+	<h1 class="text-3xl font-bold text-center m-4 md:m-6">Welcome HC15Alliance</h1>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
