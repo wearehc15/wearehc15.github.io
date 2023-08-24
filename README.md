04ccea2ef136eb205c43d72b5c24bbb74d64f491
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-08-24 20:06:12 +0200

    feat: update favicon & readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 <h1>Welcome HC15Alliance</h1>
 
-## Deployment
+The project is built using Svelte + SvelteKit.
 
-Now you just need to run `npm run gh-pages`.
+## GitHub
+[We are HC15 GitHub](https://github.com/wearehc15/wearehc15.github.io).
+
+
+## Deployment
+Run `npm run gh-pages` and check the latest build & deployment, here:
+[We are HC15 build & deployment](https://github.com/wearehc15/wearehc15.github.io/commits/gh-pages)
+
+When the checks are green, check the page: [We are HC15 online](https://wearehc15.github.io/)
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,21 +8,21 @@
   </header> -->
 
 	<main class="flex items-center justify-center h-screen">
-    <div>
-      <h1 class="text-8xl tracking-wide text-orange-900 mb-4">HC15 Alliance</h1>
-      <p class="text-2xl">We are HC15 - a collaborative alliance of entrepreneurs</p>
+    <div class="text-center">
+      <h1 class="text-6xl lg:text-8xl tracking-wide text-orange-900 mb-4">HC15 Alliance</h1>
+      <p class="lg:text-2xl">We are HC15 - a collaborative alliance of entrepreneurs</p>
     </div>
 		<!-- <slot /> -->
 	</main>
 
 	<footer>
-		<p>© 2023 HC15Alliance - Coming soon...</p>
+		<p class="text-sm">© 2023 HC15Alliance - Coming soon...</p>
 	</footer>
 </div>
 
 <style lang="postcss">
   :global(html) {
-    background: rgb(254,232,206);
-    background: linear-gradient(45deg, rgba(254,232,206,1) 0%, rgba(255,241,223,1) 100%);
+    background: #dacfc3;
+    background: linear-gradient(45deg, #fedab0 0%, #eee0cb 50%,  #fff 100%);
   }
 </style>
