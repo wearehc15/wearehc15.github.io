68bf43ffc988f2fe68bf978f00b617a7271e70db
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-08-30 18:51:49 +0200

    feat: update the why with our Alliance

--- src/routes/why/+page.svelte
+++ src/routes/why/+page.svelte
@@ -14,3 +14,15 @@
 		</div>
 	</div>
 </article>
+
+<article class="py-14 md:py-16 lg:py-24 bg-[#e9e9e1] overflow-x-hidden">
+	<div class="max-w-4xl mx-auto px-4">
+		<h1 class="md:text-center text-4xl md:text-6xl lg:text-7xl tracking-wide mb-4 lg:mb-11">Our Alliance</h1>
+
+		<p class="md:text-center text-md md:text-xl lg:text-2xl !leading-normal">
+			The HC15 is a <strong class="italic">European-based alliance of 13 diverse and accomplished women entrepreneurs</strong> who came together with the shared goals of collaboration, empowerment and support.
+		</p>
+
+		<p class="mt-4 lg:mt-9 text-center"><a href="/alliance" class="button">Learn more</a></p>
+	</div>
+</article>
