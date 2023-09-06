aa7a9ec6f3b8963affbf1eb88ea43fef23fdf914
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-09-06 19:56:31 +0200

    feedback: FAQ

--- src/routes/Faq.svelte
+++ src/routes/Faq.svelte
@@ -9,55 +9,52 @@
 		<div>
 			<Expandable title="How did we become HC15?">
 				<p class="mb-4 lg:mb-6">
-					In the heart of our journey lies a collective of 15 remarkable fempreneurs, bound by the unbreakable bonds woven during our time as participants in the EU Fellowship program, The Break. The impact of this experience was profound, touching each of us in ways words struggle to capture. It was more than a program; it was a transformational odyssey that rewrote the stories of our lives.
+					The synergy and our connection was remarkable and we took a step forward. A step not only towards our own dreams, but towards the dreams of countless others like us. We created a path to build, to expand, to grow - an opportunity for every entrepreneur.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					Such was the resonance, the synergy, the magic of our connection, that we took an awe-inspiring step forward. A step not only towards our dreams but towards the dreams of countless others like us. We forged a path to build, to expand, to grow - an opportunity wrapped in the threads of our collective experiences.
+					From the echoes of <a href="https://the-break.eu/" target="_blank">The Break</a>, the HC15 Alliance was born.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					From the echoes of that transformative journey, the HC15 Alliance was born. It's a radiant tapestry, woven with the lessons, dreams, and aspirations we carried. This alliance, firmly rooted in the soil of Europe, bears the marks of our struggles, the fragrance of our victories, and the echoes of our shared stories.
-				</p>
-				<p class="mb-4 lg:mb-6">
-					We stand here, not just as fempreneurs, but as guardians of possibility, architects of empowerment, and advocates of dreams. Each step forward is now a testament to the power of unity, the strength of collaboration, and the beauty of shared experiences. As we look ahead, we're not merely building an alliance; we're weaving a legacy, painting the skies with the hues of our camaraderie.
+					We stand here, not just as fempreneurs, but as guardians of possibility, architects of empowerment, and advocates of dreams. Each step forward is now a testament to the power of unity, the strength of collaboration, and the beauty of complementary business abilities. As we look ahead, we see many similar to us - growing in a sustainable, joyful way!
 				</p>
 			</Expandable>
 
-			<Expandable title="Who are we looking for?">
+			<Expandable title="Who can we help?">
 				<p class="mb-4 lg:mb-6">
-					At HC15, our purpose resonates with passionate entrepreneurs who yearn for more than just success - they seek growth that's vibrant, joyful, and in harmony with their well-being. Our mission pulses with the heartbeat of conscious entrepreneurship, where business isn't just a pursuit, but a journey of the heart.
+					Our mission pulses with the heartbeat of conscious entrepreneurship, where business is a well designed structure as well as vibrant passion. At HC15, our purpose resonates with passionate entrepreneurs who seek for more than just success - they look for growth in harmony with their well-being.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					We're on the lookout for those driven by purpose, those who understand that true success blooms when the mind, body, and soul thrive together. Our canvas is the world of heart-centered business growth, where each stroke of effort is a brushstroke of passion, each achievement a masterpiece of dedication.
+					We're on the lookout for those driven by purpose, those who understand that true success blooms when the mindseta and passion thrive together. At HC15, we're not just about nurturing businesses; we're about nurturing goals.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					At HC15, we're not just about nurturing businesses; we're about nurturing dreams, fostering wellness, and creating a tapestry where both prosperity and health intertwine. Join us in this voyage of transformation, where your business doesn't just flourish - it flourishes exuberantly, radiantly, and joyfully. Together, let's paint the sky with the colors of purposeful success and create a symphony of fulfillment that echoes far beyond the realms of entrepreneurship.
+					Join us on your journey of transformation. Together, let's plan your next steps, let's unstuck you, let's find your purpose and draw a clear path to your goal. We can help you in every step of the way.
 				</p>
 			</Expandable>
 
 			<Expandable title="How much does it cost?">
 				<p class="mb-4 lg:mb-6">
-					As we deeply delve into your business, our offerings are tailored to fit you perfectly. Get in touch with us to receive your personalized quote at <a href="mailto:wearehc15@gmail.com">wearehc15@gmail.com</a>.
+					As we deeply dive into your business situation, our offerings are tailoredo fit you perfectly. Get in touch with us to receive your personalized quote at <a href="mailto:wearehc15@gmail.com" target="_blank">wearehc15@gmail.com</a>.
 				</p>
 			</Expandable>
 
 			<Expandable title="Why should you do Co-creation with us?">
 				<p class="mb-4 lg:mb-6">
-					Because we truly empathize with you, the passionate entrepreneur. We comprehend that the road ahead is often shrouded in uncertainty, and that's precisely why we stand beside you - to illuminate the way, to harness your boundless energy and innate talents in a manner that resonates with your very core. Our commitment is to be your compass, guiding you through the labyrinth of possibilities with unwavering support and genuine understanding.
+					Because we truly empathize with you, The Entrepreneur. We comprehend that the road ahead is often shrouded in uncertainty, and that's precisely why we stand beside you. Our commitment is to be your compass, your support and true advisor.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					What we offer is more than just a service; it's an embrace, a hand reaching out to steady you on your remarkable journey. Our purpose is to infuse your steps with purpose and direction, to transform challenges into stepping stones, and to celebrate every milestone you conquer.
+					What we offer is more than just a service. Our purpose is to lead your steps with purpose and direction. Transform challenges into milestones and reachable goals.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					Embarking on the entrepreneurial voyage is an expedition filled with twists and turns. Yet, fear not, for we are your steadfast companions. Our collaborative strength unites to provide an all-encompassing, seamless sanctuary for entrepreneurs, carefully woven from our diverse expertise, resources, and unique offerings. In this alliance, barriers dissolve and gaps bridge, creating a nurturing haven that caters to your every entrepreneurial need.
+					Our collaborative strengths and skills unite to provide a full plan aligned with your time, schedules, true purpose and financial possibilities. In this alliance, barriers dissolve and a clear future appears.
 				</p>
 				<p class="mb-4 lg:mb-6">
-					Imagine a realm where clarity intertwines with ambition, where tailored solutions breathe life into your aspirations. Here, the journey towards alignment and accelerated growth finds its sanctuary. With us, you're not alone; you're part of a collective force, propelling you forward with holistic empowerment.
+					With us, you're not alone; you're part of a collective force, propelling you forward with holistic empowerment. We are sure you have never experienced such support before.
 				</p>
 			</Expandable>
 
-			<Expandable title="What kind of talents/expertise do you have?">
+			<Expandable title="What kind of talents/expertise do we have?">
 				<p class="mb-4 lg:mb-6">
-					We are a diverse collective of 13 fempenuress with a range of expertise from product design, finance, value proposition, UX design, engineering, business development, SEO, Data, and Permaculture design you can learn more about us on our alliance page but in short, we have everything you need to significantly catapult your projects by 10x. The best part, unlike an agency, we are more than colleagues, we are sisters united.
+					We are a diverse collective of 13 fempenuress with a range of expertise from strategy plans, business development, product design, finance, value proposition to UX design, SEO, Data, and Permaculture design. You can learn more about us on our alliance page, where you can find all our profiles, but in short, we are sure we have everything you need to significantly catapult your projects by 10x. The best part, unlike an agency, we are more than colleagues, we are sisters united.
 				</p>
 			</Expandable>
 
