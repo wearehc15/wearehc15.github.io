119aae1a8f40022305a2c05edf9fc57c0ea0ace6
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-09-07 07:53:35 +0200

    fix: madelin & remove PersonDetail

--- src/routes/PersonTitle.svelte
+++ src/routes/PersonTitle.svelte
@@ -7,7 +7,7 @@
 <div class="md:mt-6 lg:mt-16 px-4 flex-col lg:flex-row flex gap-4 lg:gap-8 max-w-4xl mx-auto mb-6 lg:mb-10">
 	<div class="w-24 h-24 mx-auto lg:w-1/3 lg:h-64 relative">
 		<div class="w-full h-full absolute overflow-hidden rounded-md">
-			<img src="{ image_url }" alt="{ firstname } { lastname }"  class="w-full h-full grayscale group-hover:grayscale-0 absolute top-0 right-0 left-0 bottom-0 object-cover" />
+			<img src="{ image_url }" alt="{ firstname } { lastname }"  class="w-full grayscale group-hover:grayscale-0 absolute top-0 right-0 left-0 bottom-0" />
 		</div>
 	</div>
 
